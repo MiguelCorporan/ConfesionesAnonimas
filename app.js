7d932e4f92a1740ac4a1f682f0393386f4507ff3
Miguel corporan <Mcorporan536@gmail.com>
2022-12-15 21:37:38 -0400

    ojala10

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 console.log(process.env.PORT);
 
